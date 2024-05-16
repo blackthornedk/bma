1f12756b8438fb16832afcf0fb26fe0c372a2328
Thomas Steen Rasmussen <tykling@bornhack.org>
2024-05-16 17:55:07 +0200

    use linkify=True to render file detail link

--- src/files/tables.py
+++ src/files/tables.py
@@ -1,7 +1,5 @@
 """This module defines the table used to show files."""
 import django_tables2 as tables
-from django.urls import reverse
-from django.utils.html import format_html
 
 from .models import BaseFile
 
@@ -10,6 +8,7 @@ class FileTable(tables.Table):
     """Defines the django-tables2 used to show files."""
 
     selection = tables.CheckBoxColumn(accessor="pk", orderable=False)
+    uuid = tables.Column(linkify=True)
 
     class Meta:
         """Define model, template, fields."""
@@ -17,8 +16,3 @@ class FileTable(tables.Table):
         model = BaseFile
         template_name = "django_tables2/bootstrap.html"
         fields = ("uuid", "albums", "attribution", "uploader", "license", "file_size", "status", "title")
-
-    def render_uuid(self, value: str) -> str:
-        """Render column data with 'a' tag and return as formatted html."""
-        url = reverse("files:detail", kwargs={"pk": value})
-        return format_html(f"<a href='{url}'>{value}</a>")
